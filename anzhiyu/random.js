8edf5b56c9320f15140819350fa663b3d7d63147
PeachPeach-AI <15791923693@139.com>
2024-08-06 18:03:10 +0800

    Site updated: 2024-08-06 18:03:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/16/TEST/","2024/07/16/hello-world/","2024/07/27/test2/"];function toRandomPost(){
+var posts=["2024/07/27/test2/","2024/07/16/hello-world/","2024/07/16/TEST/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
