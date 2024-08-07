65c50b4bc58470666cd7890c3741cd4d0345b116
PeachPeach-AI <15791923693@139.com>
2024-08-07 17:24:05 +0800

    Site updated: 2024-08-07 17:24:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/16/hello-world/","2024/07/27/test2/","2024/07/16/TEST/","2024/07/16/7步搞定，高转化！促销商品主图工单模板详解/"];function toRandomPost(){
+var posts=["2024/07/16/TEST/","2024/07/16/hello-world/","2024/07/27/test2/","2024/07/16/7步搞定，高转化！促销商品主图工单模板详解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
