3a820fd6323b28f388b9288d9bd7a6d475973c63
PeachPeach-AI <15791923693@139.com>
2024-08-22 23:00:42 +0800

    Site updated: 2024-08-22 23:00:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/16/7步搞定，高转化！促销商品主图工单模板详解/"];function toRandomPost(){
+var posts=["2024/08/13/page/","2022/08/16/7步搞定，高转化！促销商品主图工单模板详解 - 副本/","2024/07/16/7步搞定，高转化！促销商品主图工单模板详解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
